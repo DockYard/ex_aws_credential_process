9618183a52b09c304dfa0c8ac70cd77603ce2809
Nathan Long <him@nathanmlong.com>
2020-03-30 15:57:52 -0400

    Declare version 1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExAwsCredentialProcess.MixProject do
   def project do
     [
       app: :ex_aws_credential_process,
-      version: "0.4.0",
+      version: "1.0.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
